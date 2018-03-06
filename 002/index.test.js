665e5794bb0c9128ca4c3878bdf1c4b4f35b7759
Isaac Dickson <tdickson9@gmail.com>
2018-03-05 20:23:38 -0800

    test file for euler 002, fibonacci sequence addermajig

---
+++ 002/index.test.js
@@ -0,0 +1,34 @@
+
+var euler = require('./index');
+
+describe('euler', function () {
+
+  it('lists all numbers in the Fibonacci sequence below 100', function () {
+    var expected = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] 
+    expect(euler.fibonacci(100)).toEqual(expected);
+  });
+  xit('lists all numbers in the Fibonacci sequence below 60', function () {
+    var expected = [1, 2, 3, 5, 8, 13, 21, 34, 55] 
+    expect(euler.fibonacci(60)).toEqual(expected);
+  });
+
+  xit('returns the sum of even terms within the array', function () {
+    var expected = 44
+    expect(euler.sumOfEvens([1, 2, 3, 5, 8, 13, 21, 34, 55])).toEqual(expected);
+  });
+
+  xit('returns the sum of even terms within the array', function () {
+    var expected = 18
+    expect(euler.sumOfEvens([3, 4, 6, 9, 8])).toEqual(expected);
+  });
+  xit('returns the sum of even terms in the Fibonacci sequence below 100', function () {
+    var expected = 44
+    expect(euler.sumOfEvenFibonacci(100)).toEqual(expected);
+  });
+
+  xit('returns the sum of even terms in the Fibonacci sequence below 30', function () {
+    var expected = 10
+    expect(euler.sumOfEvenFibonacci(30)).toEqual(expected);
+  });
+	
+});
