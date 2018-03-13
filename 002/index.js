9ef1b5ed64d52d198c52739cac93fec10466dd48
Isaac Dickson <tdickson9@gmail.com>
2018-03-12 20:11:19 -0700

    added fibonacci and sumOfEvenFibonacci functions

--- 002/index.js
+++ 002/index.js
@@ -1,3 +1,17 @@
+function fibonacci(maxFibonacci){
+	
+	var fibs = [1,2]
+	var i = 0
+	var sequenceNum = 3
+	while (sequenceNum < maxFibonacci){
+		fibs.push(sequenceNum)
+		i++
+		sequenceNum = fibs[i] + fibs[i+1]
+	//take index 0 and 1 add them together, take result and push it to the array
+	}
+	return fibs
+}
+
 function sumOfEvens(array){
   var evenNum = 0
   for (var i = 0; i < array.length; i++){
@@ -8,10 +22,13 @@ function sumOfEvens(array){
 	return evenNum
 }
 
+function sumOfEvenFibonacci(maxFibonacci){
+	return sumOfEvens(fibonacci(maxFibonacci))
+}
 
 
 module.exports = {
-	//fibonacci: fibonacci,
+	fibonacci: fibonacci,
 	sumOfEvens: sumOfEvens,
-	//sumOfEvenFibonacci: sumOfEvenFibonacci
+	sumOfEvenFibonacci: sumOfEvenFibonacci
 }
--- 002/index.test.js
+++ 002/index.test.js
@@ -3,11 +3,11 @@ var euler = require('./index');
 
 describe('euler', function () {
 
-  xit('lists all numbers in the Fibonacci sequence below 100', function () {
+  it('lists all numbers in the Fibonacci sequence below 100', function () {
     var expected = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] 
     expect(euler.fibonacci(100)).toEqual(expected);
   });
-  xit('lists all numbers in the Fibonacci sequence below 60', function () {
+  it('lists all numbers in the Fibonacci sequence below 60', function () {
     var expected = [1, 2, 3, 5, 8, 13, 21, 34, 55] 
     expect(euler.fibonacci(60)).toEqual(expected);
   });
@@ -21,12 +21,12 @@ describe('euler', function () {
     var expected = 18
     expect(euler.sumOfEvens([3, 4, 6, 9, 8])).toEqual(expected);
   });
-  xit('returns the sum of even terms in the Fibonacci sequence below 100', function () {
+  it('returns the sum of even terms in the Fibonacci sequence below 100', function () {
     var expected = 44
     expect(euler.sumOfEvenFibonacci(100)).toEqual(expected);
   });
 
-  xit('returns the sum of even terms in the Fibonacci sequence below 30', function () {
+  it('returns the sum of even terms in the Fibonacci sequence below 30', function () {
     var expected = 10
     expect(euler.sumOfEvenFibonacci(30)).toEqual(expected);
   });
