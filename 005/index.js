8c6680b506279bcb60e8a5336e22c75c231c808c
Isaac Dickson <tdickson9@gmail.com>
2018-04-18 19:35:47 -0700

    Created divisibleTest function

--- 005/index.js
+++ 005/index.js
@@ -1,5 +1,13 @@
-function divisibleTest(){
-  
+function divisibleTest(largestInput){
+  for(i=1; true; i++){
+    var isSmallestMultiple = true;
+    for(j=largestInput; j > 1; j--){
+      isSmallestMultiple = isSmallestMultiple && i % j == 0;
+    }
+    if(isSmallestMultiple){
+      return i // if this AND that
+    }  
+  }
 }
 
 module.exports = {
